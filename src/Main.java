587e019bb4c78f8f95afe1e236b0993d36f60f74
nicholassamuelssonjeria <nicholasjobb.jeria@gmail.com>
2024-10-16 22:39:47 +0200

    Testet arrayList

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,7 @@ public class Main {
                 new Pet("Sara", "Cat"),
                 new Pet("Bob", "Dog"),
         };
+
 //        To get the value
         System.out.println(friendsArray1[0]);
 //        In a forEach
@@ -28,15 +29,40 @@ public class Main {
         for (int i = 0; i < myPets.length; i++) {
             System.out.println(myPets[i].name + " is " + myPets[i].type);
         }
+        System.out.println("--------------------------------------");
+        System.out.println(friendsArray.length);
+        System.out.println(myPets.length);
 
 
-
+        System.out.println("--------------------------------------");
 
 //        ArrayList - Grows and shrinks automatically, has no size limit
 //        ArrayList - can only store objects
         ArrayList<String> friendsArrayList = new ArrayList<>();
         ArrayList<String> friendsArrayList1 = new ArrayList<>(Arrays.asList("Luke", "Yoda", "Leia"));
+        System.out.println(friendsArrayList1.get(0));
+        System.out.println(friendsArrayList1.get(1));
 
+        System.out.println(friendsArrayList1.size());
+        System.out.println("--------------------------------------");
 
+//        Set element
+        friendsArray1[0] = "nicholas";
+        System.out.println(friendsArray1[0]);
+        System.out.println("--------------------------------------");
+        friendsArrayList1.forEach(System.out::println);
+
+        Pet dog = new Pet("Buster", "Dog");
+        Pet cat = new Pet("Cat", "Cat");
+        ArrayList<Pet> pets = new ArrayList<>();
+        pets.add(dog);
+        pets.add(cat);
+
+        for (Pet pet : pets) {
+            System.out.println(pet.name + " is " + pet.type);
+        }
+        System.out.println("--------------------------------------");
+        ArrayList<String> names = new ArrayList<>(Arrays.asList("Nicholas", "Selma", "Jennifer", "Wilda"));
+        names.forEach(System.out::println);
     }
 }
